58ffa88395811d63fc2270ff94c511733f5a4ddb
Shoadi Yokoyama <quantumcars@gmail.com>
2014-04-03 00:10:11 +0900

    Marshals: Make CString instance of Marshal

--- src/Foreign/Marshals.hs
+++ src/Foreign/Marshals.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Foreign.Marshals
@@ -5,7 +6,8 @@ module Foreign.Marshals
          Marshal(..)
        ) where
 
-import Foreign.C (CInt)
+import Foreign (free)
+import Foreign.C
 
 import Foreign.Marshals.Class
 
@@ -14,3 +16,10 @@ instance Marshal CInt where
   type Haskell CInt = Int
   toHaskell   = return . fromIntegral
   fromHaskell = return . fromIntegral
+
+instance Marshal CString where
+  type Haskell CString = String
+  toHaskell   = peekCString
+  fromHaskell = newCString
+  withHaskell = withCString
+  cleanup     = free
