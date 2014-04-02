415d7471ace72bb3973c57dd1ae3f5be0042f399
Shoadi Yokoyama <quantumcars@gmail.com>
2014-04-02 22:40:56 +0900

    Marshals: Make CInt instance of Marshal

--- src/Foreign/Marshals.hs
+++ src/Foreign/Marshals.hs
@@ -1,3 +1,16 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module Foreign.Marshals
        (
+         Marshal(..)
        ) where
+
+import Foreign.C (CInt)
+
+import Foreign.Marshals.Class
+
+
+instance Marshal CInt where
+  type Haskell CInt = Int
+  toHaskell   = return . fromIntegral
+  fromHaskell = return . fromIntegral
