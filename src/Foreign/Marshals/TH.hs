c78d7fd5670ae81c39f6e463c450325bdfd6fc03
Shoadi Yokoyama <quantumcars@gmail.com>
2014-04-18 09:02:15 +0900

    TH: Add mkMarshalInstance

--- src/Foreign/Marshals/TH.hs
+++ src/Foreign/Marshals/TH.hs
@@ -2,4 +2,21 @@
 
 module Foreign.Marshals.TH
        (
+         mkMarshalInstance
        ) where
+
+import Language.Haskell.TH
+
+import Foreign.Marshals.Class
+
+-- Declare instances for c number types, ie. CChar, CShort and so on.
+mkMarshalInstance :: Name -> Name -> Q [Dec]
+mkMarshalInstance ctype htype = [d|
+    instance Marshal $fromT where
+      type Haskell $fromT = $toT
+      toHaskell   = return . fromIntegral
+      fromHaskell = return . fromIntegral
+  |]
+  where
+    fromT = conT ctype
+    toT = conT htype
